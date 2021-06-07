b629a3daf8acb40ba1783290269b2e81a327d99a
Maciek2k3 <maciej.palczewski88@gmail.com>
2021-06-07 20:56:27 +0200

    first commit

---
+++ src/main/java/com/palczewski/chat/ChatMessageController.java
@@ -0,0 +1,18 @@
+package com.palczewski.chat;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatMessageController {
+
+    @MessageMapping("/chat")
+    @SendTo("/topic/messages")
+    public ChatMessage get(ChatMessage chatMessage){
+        return chatMessage;
+
+    }
+
+
+}
